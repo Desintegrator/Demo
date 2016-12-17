1a39b3b00022683233286db03d968886ed63a6a2
Danila Nikonets <danila.nikonets@gmail.com>
2016-12-18 00:23:29 +0500

    first commit

---
+++ MyClass.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyProgram
+{
+	class Program
+	{
+     	void Main (string[] args)
+     	{
+     		Console.WriteLine("Hello World!");
+     		Console.ReadLine();
+     	}
+	}
+}
